5c3e2cd8b389e77e5ed5d58d9b6a2b9c89a3e0ca
Stanislau Dykomenko <stanislau.dykomenko@effective-soft.com>
2022-03-16 14:29:18 +0300

    type of useRef hook

--- src/components/EventsExample.tsx
+++ src/components/EventsExample.tsx
@@ -1,14 +1,15 @@
-import React, { FC, useState } from "react";
+import React, { FC, useRef, useState } from "react";
 
 const EventsExample: FC = () => {
   const [value, setValue] = useState<string>("");
   const [isDrag, setIsDrag] = useState<boolean>(false);
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const changeHandler = (e: React.ChangeEvent<HTMLInputElement>) =>
     setValue(e.target.value);
 
   const clickHandler = (e: React.MouseEvent<HTMLButtonElement>) =>
-    console.log(value);
+    console.log(inputRef.current?.value);
 
   const dragHandler = (e: React.DragEvent<HTMLDivElement>) =>
     console.log("DRAG");
@@ -31,7 +32,13 @@ const EventsExample: FC = () => {
 
   return (
     <div>
-      <input value={value} onChange={changeHandler} type="text" />
+      <input
+        value={value}
+        onChange={changeHandler}
+        type="text"
+        placeholder="Управляемый"
+      />
+      <input ref={inputRef} type="text" placeholder="Неуправляемый" />
       <button onClick={clickHandler}>click</button>
       <div
         onDrag={dragHandler}
